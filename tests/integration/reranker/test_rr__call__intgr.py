25831de8d39a1f73ae50c36223bf7fc9a083a5c1
rlucatoor <rlucato95@gmail.com>
2025-11-14 18:09:55 +0100

    add integration tests

--- tests/integration/reranker/test_rr__call__intgr.py
+++ tests/integration/reranker/test_rr__call__intgr.py
@@ -4,17 +4,21 @@ from artifex import Artifex
 
 
 @pytest.mark.integration
-def test_train_success(
+def test_train_single_input_success(
     artifex: Artifex
 ):
     """
-    Test the `__call__` method of the `Reranker` class. Ensure that the return type is
-    list[tuple[str, float]].
+    Test the `__call__` method of the `Reranker` class when a single input is provided. 
+    Ensure that: 
+    - The return type is list[tuple[str, float]].
+    - The output tuples only contain the provided input documents
     Args:
         artifex (Artifex): The Artifex instance to be used for testing.
     """
     
-    out = artifex.reranker(query="test query", documents=["doc1", "doc2"])
+    input_doc = "doc1"
+    
+    out = artifex.reranker(query="test query", documents=input_doc)
     assert isinstance(out, list)
     assert all(
         isinstance(resp, tuple) and 
@@ -22,3 +26,29 @@ def test_train_success(
         isinstance(resp[1], float) 
         for resp in out
     )
+    assert all(resp[0] in [input_doc] for resp in out)
+    
+@pytest.mark.integration
+def test_train_multiple_inputs_success(
+    artifex: Artifex
+):
+    """
+    Test the `__call__` method of the `Reranker` class when multiple inputs are provided. 
+    Ensure that: 
+    - The return type is list[tuple[str, float]].
+    - The output tuples only contain the provided input documents.
+    Args:
+        artifex (Artifex): The Artifex instance to be used for testing.
+    """
+    
+    input_docs = ["doc1", "doc2", "doc3"]
+    
+    out = artifex.reranker(query="test query", documents=input_docs)
+    assert isinstance(out, list)
+    assert all(
+        isinstance(resp, tuple) and 
+        isinstance(resp[0], str) and 
+        isinstance(resp[1], float) 
+        for resp in out
+    )
+    assert all(resp[0] in input_docs for resp in out)
--- tests/integration/sentiment_analysis/test_sa__call__.py
+++ tests/integration/sentiment_analysis/test_sa__call__.py
@@ -4,17 +4,40 @@ from artifex import Artifex
 from artifex.core import ClassificationResponse
 
 
+expected_labels = ["very_positive", "positive", "negative", "very_negative", "neutral"]
+
+
 @pytest.mark.integration
-def test_train_success(
+def test_train_single_input_success(
     artifex: Artifex
 ):
     """
-    Test the `__call__` method of the `SentimentAnalysis` class. Ensure that it returns a list of 
-    ClassificationResponse objects.
+    Test the `__call__` method of the `SentimentAnalysis` class. Ensure that: 
+    - It returns a list of ClassificationResponse objects.
+    - The output labels are among the expected sentiment labels.
     Args:
         artifex (Artifex): The Artifex instance to be used for testing.
     """
     
     out = artifex.sentiment_analysis("test input")
     assert isinstance(out, list)
+    assert all(resp.label in expected_labels for resp in out)
+    assert all(isinstance(resp, ClassificationResponse) for resp in out)
+    
+@pytest.mark.integration
+def test_train_multiple_inputs_success(
+    artifex: Artifex
+):
+    """
+    Test the `__call__` method of the `SentimentAnalysis` class, when multiple inputs are 
+    provided. Ensure that: 
+    - It returns a list of ClassificationResponse objects.
+    - The output labels are among the expected sentiment labels.
+    Args:
+        artifex (Artifex): The Artifex instance to be used for testing.
+    """
+    
+    out = artifex.sentiment_analysis(["test input 1", "test input 2", "test input 3"])
+    assert isinstance(out, list)
+    assert all(resp.label in expected_labels for resp in out)
     assert all(isinstance(resp, ClassificationResponse) for resp in out)
