e26b097dc2070e236b25089e8d8bb599d2b09583
rlucatoor <rlucato95@gmail.com>
2025-11-16 16:51:02 +0100

    fix broken unit test

--- tests/unit/sentiment_analysis/test_sa_get_data_gen_instr.py
+++ tests/unit/sentiment_analysis/test_sa_get_data_gen_instr.py
@@ -29,11 +29,12 @@ def test_get_data_gen_instr_success(
         artifex (Artifex): An instance of the Artifex class with a sentiment_analysis attribute.
     """
 
-    user_instr_1, user_instr_2 = "user instruction 1", "user instruction 2"
+    user_instr_1, user_instr_2, user_instr_3 = "user instruction 1", "user instruction 2", "user instruction 3"
     
     user_instructions: list[str] = [
         user_instr_1,
         user_instr_2,
+        user_instr_3,
     ]
     
     sentiment_analysis = artifex.sentiment_analysis
@@ -41,6 +42,11 @@ def test_get_data_gen_instr_success(
     
     # Assert that the combined instructions are a list with the expected format
     assert isinstance(combined_instr, list)
-    assert len(combined_instr) == len(sentiment_analysis._system_data_gen_instr) + len(user_instructions) # type: ignore
+    # The length of the combined instructions should equal the sum of system and user instructions minus 1
+    # (since the last user instruction is inserted into the system prompt, while the others are appended)
+    assert len(combined_instr) == len(sentiment_analysis._system_data_gen_instr) + len(user_instructions) -1 # type: ignore
+    # The last two user instructions should be at the end of the list
     assert combined_instr[-2] == user_instr_1
     assert combined_instr[-1] == user_instr_2
+    # The first user instruction should be inserted into the system propmpt's first string.
+    assert user_instr_3 in combined_instr[0]
