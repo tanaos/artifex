6f918ad30877a796e26a003e7a10132507e5a9e9
rlucatoor <rlucato95@gmail.com>
2025-11-17 18:58:58 +0100

    add unit tests for EmotionDetection._get_data_gen_instr

---
+++ tests/unit/emotion_detection/test_ed_get_data_gen_instr.py
@@ -0,0 +1,52 @@
+import pytest
+
+from artifex import Artifex
+from artifex.core import ValidationError
+
+
+@pytest.mark.unit
+def test_get_data_gen_instr_validation_failure(
+    artifex: Artifex
+):
+    """
+    Test that the `_parse_user_instructions` method of the `EmotionDetection` class raises a 
+    ValidationError when provided with invalid user instructions.
+    Args:
+        artifex (Artifex): The Artifex instance under test.
+    """
+        
+    with pytest.raises(ValidationError):
+        artifex.emotion_detection._get_data_gen_instr("invalid instructions") # type: ignore
+
+
+def test_get_data_gen_instr_success(
+    artifex: Artifex
+):
+    """
+    Test that the _get_data_gen_instr method of the `EmotionDetection` class correctly combines
+    system and user instructions into a single list.
+    Args:
+        artifex (Artifex): An instance of the Artifex class.
+    """
+
+    user_instr_1, user_instr_2, user_instr_3 = "user instruction 1", "user instruction 2", "user instruction 3"
+    
+    user_instructions: list[str] = [
+        user_instr_1,
+        user_instr_2,
+        user_instr_3,
+    ]
+    
+    emotion_detection = artifex.emotion_detection
+    combined_instr = emotion_detection._get_data_gen_instr(user_instructions) # type: ignore
+    
+    # Assert that the combined instructions are a list with the expected format
+    assert isinstance(combined_instr, list)
+    # The length of the combined instructions should equal the sum of system and user instructions minus 1
+    # (since the last user instruction is inserted into the system prompt, while the others are appended)
+    assert len(combined_instr) == len(emotion_detection._system_data_gen_instr) + len(user_instructions) -1 # type: ignore
+    # The last two user instructions should be at the end of the list
+    assert combined_instr[-2] == user_instr_1
+    assert combined_instr[-1] == user_instr_2
+    # The first user instruction should be inserted into the system propmpt's first string.
+    assert user_instr_3 in combined_instr[0]
