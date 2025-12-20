d570d07ad878156cfbd7c13a8865a3f5c0fc26ee
rlucatoor <rlucato95@gmail.com>
2025-12-20 15:40:23 +0100

    add integration tests for ClassificationModel class

---
+++ tests/integration/text_classification/test_tc_train_intgr.py
@@ -0,0 +1,30 @@
+import pytest
+
+from artifex import Artifex
+
+
+@pytest.mark.integration
+def test_train_success(
+    artifex: Artifex
+):
+    """
+    Test the `train` method of the `ClassificationModel` class. Verify that:
+    - The training process completes without errors.
+    Args:
+        artifex (Artifex): The Artifex instance to be used for testing.
+    """
+    
+    classes = {
+        "class_a": "Description for class A.",
+        "class_b": "Description for class B.",
+        "class_c": "Description for class C."
+    }
+    
+    tc = artifex.text_classification
+    
+    tc.train(
+        domain="test domain",
+        classes=classes,
+        num_samples=40,
+        num_epochs=1
+    )
