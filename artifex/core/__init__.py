9718eefdc041f9c09f89d64d287c4c2e2310a2e4
rlucatoor <rlucato95@gmail.com>
2026-01-17 21:32:42 +0100

    turn raw dictionaries into pydantic models in logging.py

--- artifex/core/__init__.py
+++ artifex/core/__init__.py
@@ -1,13 +1,28 @@
 from .decorators import auto_validate_methods, track_inference_calls, track_training_calls
 from .exceptions import ServerError, ValidationError, BadRequestError
-from .models import ClassificationResponse, ClassificationClassName, NERTagName, NEREntity, \
-    ClassificationInstructions, NERInstructions, ParsedModelInstructions
+from .models import (
+    ClassificationResponse, 
+    ClassificationClassName, 
+    NERTagName, 
+    NEREntity,
+    ClassificationInstructions, 
+    NERInstructions, 
+    ParsedModelInstructions, 
+    Warning,
+    InferenceLogEntry,
+    InferenceErrorLogEntry,
+    DailyInferenceAggregateLogEntry,
+    TrainingLogEntry,
+    TrainingErrorLogEntry,
+    DailyTrainingAggregateLogEntry
+)
 from .log_shipper import initialize_log_shipper, ship_log, get_log_shipper
 
 
 __all__ = [
     "auto_validate_methods",
     "track_inference_calls",
+    "track_training_calls",
     "ServerError", 
     "ValidationError", 
     "BadRequestError",
@@ -18,6 +33,13 @@ __all__ = [
     "ClassificationInstructions",
     "NERInstructions",
     "ParsedModelInstructions",
+    "Warning",
+    "InferenceLogEntry",
+    "InferenceErrorLogEntry",
+    "DailyInferenceAggregateLogEntry",
+    "TrainingLogEntry",
+    "TrainingErrorLogEntry",
+    "DailyTrainingAggregateLogEntry",
     "initialize_log_shipper",
     "ship_log",
     "get_log_shipper"
--- artifex/core/models.py
+++ artifex/core/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Literal, Any
 
 from artifex.config import config
 
@@ -67,3 +67,85 @@ class ParsedModelInstructions(BaseModel):
     user_instructions: list[str]
     domain: Optional[str] = None
     language: str
+
+WarningType = Literal[
+    # Inference warnings
+    "low_confidence_warning",
+    "slow_inference_warning", 
+    "high_token_count_warning",
+    "short_input_warning",
+    "null_output_warning",
+    # Training warnings
+    "high_training_loss_warning",
+    "slow_training_warning",
+    "low_training_throughput_warning"
+]
+
+class Warning(BaseModel):
+    entry_type: WarningType
+    warning_reason: str
+
+class InferenceLogEntry(BaseModel):
+    entry_type: Literal["inference"]
+    timestamp: str
+    model: str
+    inference_duration_seconds: float
+    cpu_usage_percent: float
+    ram_usage_percent: float
+    input_token_count: int
+    inputs: dict[str, Any]
+    output: Any
+
+class InferenceErrorLogEntry(BaseModel):
+    entry_type: Literal["inference_error"]
+    timestamp: str
+    model: str
+    error_type: str
+    error_message: str
+    error_location: Optional[dict[str, Any]]
+    inputs: dict[str, Any]
+    inference_duration_seconds: float
+
+class DailyInferenceAggregateLogEntry(BaseModel):
+    entry_type: Literal["daily_aggregate"]
+    date: str
+    total_inferences: int
+    total_input_token_count: int
+    total_inference_duration_seconds: float
+    avg_ram_usage_percent: float
+    avg_cpu_usage_percent: float
+    avg_input_token_count: float
+    avg_inference_duration_seconds: float
+    avg_confidence_score: Optional[float]
+    model_usage_breakdown: dict[str, int]
+
+class TrainingLogEntry(BaseModel):
+    entry_type: Literal["training"]
+    timestamp: str
+    model: str
+    training_duration_seconds: float
+    cpu_usage_percent: float
+    ram_usage_percent: float
+    inputs: dict[str, Any]
+    train_results: Any
+
+class TrainingErrorLogEntry(BaseModel):
+    entry_type: Literal["training_error"]
+    timestamp: str
+    model: str
+    error_type: str
+    error_message: str
+    error_location: Optional[dict[str, Any]]
+    inputs: dict[str, Any]
+    training_duration_seconds: float
+
+class DailyTrainingAggregateLogEntry(BaseModel):
+    entry_type: Literal["daily_training_aggregate"]
+    date: str
+    total_trainings: int
+    total_training_time_seconds: float
+    avg_ram_usage_percent: float
+    avg_cpu_usage_percent: float
+    avg_training_duration_seconds: float
+    avg_train_loss: Optional[float]
+    model_training_breakdown: dict[str, int]
